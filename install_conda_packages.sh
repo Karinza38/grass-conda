bec1353526ae5f47798648db3fd83c8ecf275636
Nelson Kandel <075bce084.nelson@pcampus.edu.np>
2024-10-07 11:28:39 -0600

    Reorganize conda  builder scripts

---
+++ install_conda_packages.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+conda install cmake conda-build
+
+conda install proj
+conda install libgdal
+conda install fftw
+conda install blas
+conda install libpdal
+conda install wxpython
+
+#conda install gettext
+#conda install xorg-libx11
+#conda install mesa-libgl-devel-cos7-x86_64
