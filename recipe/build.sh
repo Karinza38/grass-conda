d091cd89ebfc6a34d52bfd9dcaef64906305cf6a
Nelson Kandel <075bce084.nelson@pcampus.edu.np>
2024-10-07 11:41:08 -0600

    Specify library paths for conda build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 mkdir build
 cd build
 
-cmake -S ${SRC_DIR} -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PATH=${PREFIX}
+export CONDA_PREFIX=$(dirname $(dirname $(which cmake)))
+cmake \
+	-S "${SRC_DIR}" \
+	-DCMAKE_PREFIX_PATH="${PREFIX}" \
+	-DCMAKE_INSTALL_PATH="${PREFIX}" \
+	-DCMAKE_IGNORE_PREFIX_PATH="/usr" \
+	-DCMAKE_LIBRARY_PATH="${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64" \
+	-DCMAKE_INCLUDE_PATH="${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/include"
 
-make -j`nproc`
+#make -j`nproc`
+make
 
 echo ================== $(which cmake)
-
-
